48c188367afaf29dc616f782e468f0b123e5cedb
Sadfafdafdafaf <das00@inbox.ru>
2017-10-08 19:52:43 +0300

    Fix run server

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -13,13 +13,13 @@ namespace ConsoleApplication1
             Console.Title = "HTTP test server";
             http_server test_server = new http_server();
             commands com_pars = new commands();
-            string input;
+            string input = "run";
             int flag = 0;
 
             while (flag == 0)
             {
                 com_pars.terminate();
-                input = Console.ReadLine();
+                //input = Console.ReadLine();
                 Task<int> managetask = new Task<int>(() => commands.manager(input, test_server));
                 managetask.Start();
                 while ((managetask.Status != TaskStatus.RanToCompletion) && (managetask.Status != TaskStatus.Faulted) && (managetask.Status != TaskStatus.Canceled))
